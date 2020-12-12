65577f79e4c89291abe23edcb61d40b1b4dc4558
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2020-12-12 14:54:35 +0100

    test: add incorrect vault check case

--- tests/test_setters_getters.py
+++ tests/test_setters_getters.py
@@ -1,7 +1,15 @@
 import pytest
 import brownie
 from brownie import Wei
+from brownie import config
 
 
 def test_constructor(vault, gov, strategy, strategist):
     assert strategy.name() == "StrategyIdleUSDC_BY"
+
+def test_incorrect_vault(pm, guardian, gov, strategist, rewards, StrategyIdleUSDC_BY, Token):
+    token = guardian.deploy(Token)
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault, token, gov, rewards, "", "")
+    with brownie.reverts("Vault want is not USDC"):
+        strategy = strategist.deploy(StrategyIdleUSDC_BY, vault)
