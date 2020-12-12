3b1eab0e9483e7515e0cdb76a3d0e92807763edf
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2020-12-12 14:55:28 +0100

    fix: add StrategyIdleUSDC_BY revert on incorrect vault

--- contracts/StrategyIdleUSDC_BY.sol
+++ contracts/StrategyIdleUSDC_BY.sol
@@ -14,6 +14,7 @@ contract StrategyIdleUSDC_BY is StrategyIdle {
   address constant public __comp = address(0xc00e94Cb662C3520282E6f5717214004A7f26888);
   address constant public __idle = address(0x875773784Af8135eA0ef43b5a374AaD105c5D39e);
   address constant public __weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+  address constant public __usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
   address constant public __idleReservoir = address(0x031f71B5369c251a6544c41CE059e6b3d61e42C6);
   
@@ -36,6 +37,7 @@ contract StrategyIdleUSDC_BY is StrategyIdle {
         __uniswapRouterV2
   )
   public {
+    require(address(want) == __usdc, "Vault want is not USDC");
   }
 
   function name() external override pure returns (string memory) {
