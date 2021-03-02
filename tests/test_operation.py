32afee5c5d955ca7709ff68bc7437299484ac511
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2021-03-02 23:21:50 +0100

    test: profit for lending and dust not reported correctly

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,3 +1,7 @@
+import pytest
+import brownie
+from brownie import Wei
+
 # TODO: Add tests here that show the normal operation of this strategy
 #       Suggestions to include:
 #           - strategy loading and unloading (via Vault addStrategy/revokeStrategy)
@@ -6,3 +10,27 @@
 
 def test_operation():
     pass
+
+def test_profit_from_lending(vault, gov, strategy, token, tokenWhale, strategist, chain):
+    decimals = token.decimals()
+    token.approve(vault, 2 ** 256 - 1, {"from": tokenWhale})
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.addStrategy(strategy, 10_000, 0, 0, {"from": gov})
+    vault.deposit(100 * (10 ** decimals), {"from": tokenWhale})
+
+    idleYieldToken = strategy.idleYieldToken()
+
+    # Everything should be invested
+    idleBalanceBeforeHarvest = token.balanceOf(idleYieldToken)
+    strategy.harvest({"from": gov})
+    idleBalanceAfterHarvest = token.balanceOf(idleYieldToken)
+    assert token.balanceOf(strategy) == 0
+    assert (idleBalanceAfterHarvest-idleBalanceBeforeHarvest) == 100 * (10 ** decimals)
+
+    chain.mine(100)
+
+    strategy.harvest({"from": gov})
+    totalDebt = vault.strategies(strategy).dict()["totalDebt"]
+    estimatedTotalAssets = strategy.estimatedTotalAssets()
+
+    assert totalDebt == estimatedTotalAssets
