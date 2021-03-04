c31e5d024ee6719b57005f900fc69ef704a58d31
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2021-03-04 01:27:52 +0100

    test: add test for migration

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,7 +1,22 @@
-# TODO: Add tests that show proper migration of the strategy to a newer one
-#       Use another copy of the strategy to simulate the migration
-#       Show that nothing is lost!
+from brownie import config
 
+def test_migration(gov, strategyFactory, vault, token, tokenWhale):
+    strategyOld = strategyFactory(vault)
 
-def test_migration():
-    pass
+    decimals = token.decimals()
+    token.approve(vault, 2 ** 256 - 1, {"from": tokenWhale})
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.addStrategy(strategyOld, 10_000, 0, 0, {"from": gov})
+    vault.deposit(100 * (10 ** decimals), {"from": tokenWhale})
+
+    strategyOld.harvest({"from": gov})
+
+    estimatedTotalAssetsStrategyOld = strategyOld.estimatedTotalAssets()
+
+    strategyNext = strategyFactory(vault)
+
+    vault.migrateStrategy(strategyOld, strategyNext, {"from": gov})
+
+    estimatedTotalAssetsStrategyNext = strategyNext.estimatedTotalAssets()
+
+    assert estimatedTotalAssetsStrategyNext + 1 >= estimatedTotalAssetsStrategyOld
