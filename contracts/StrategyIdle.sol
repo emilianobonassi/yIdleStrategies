426a21ca5d94e372c7adba1c41da3ce2514ce385
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2021-04-05 19:45:51 +0200

    fix: report farming profits in the same harvest call

--- contracts/StrategyIdle.sol
+++ contracts/StrategyIdle.sol
@@ -422,7 +422,7 @@ contract StrategyIdle is BaseStrategyInitializable {
                 );
 
                 // leverage uniswap returns want amount
-                liquidated.add(amounts[path.length-1]);
+                liquidated = liquidated.add(amounts[path.length-1]);
             }
         }
     }
